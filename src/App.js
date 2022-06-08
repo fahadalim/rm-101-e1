58756d43b7d929af8f23e03aead92ff8c366893f
fahadalim <mohammadfahadalim@gmail.com>
2022-06-08 19:32:34 +0530

    com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import AddTask from "./components/AddTask/AddTask";
+import Task from "./components/Task/Task";
 function App() {
-  return <div>{/* Code Here */}</div>;
+  return <div>{/* Code Here */
+  <>
+  <AddTask></AddTask>
+  <Task></Task>
+  </>
+  }</div>;
 }
 
 export default App;
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styles from "./task.module.css";
 
 const Task = () => {
   // NOTE: do not delete `data-testid` key value pair
+  const [data,setData] = useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:8000/").then(({data})=>(
+      setData(data.users)
+    ))
+  },[])
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
       <div data-testid="task-text"></div>
       {/* Counter here */}
-      <button data-testid="task-remove-button"></button>
+      <button data-testid="task-remove-button">delete</button>
     </li>
   );
 };
